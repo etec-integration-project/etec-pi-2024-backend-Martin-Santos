9fc2ddb73d06178dd92f8b41c92a66ebc52dd226
Martin-Santos <me.santos@alumno.etec.um.edu.ar>
2024-05-24 10:40:30 -0300

    sprint 2 in progress

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-import express from 'express'
-import {createPool} from 'mysql2/promise'
-import {config} from 'dotenv'
+import express from 'express';
+import { createPool } from 'mysql2/promise';
+import { config } from 'dotenv';
+import authRutas from './rutas/authRutas.js';
+
 config();
 
-const app = express()
+const app = express();
 
-// console.log({
-//     host: process.env.MYSQLDB_HOST,
-//     password: process.env.MYSQLDB_PASSWORD,
-//     port: process.env.MYSQLDB_PORT,
-// })
-const pool = createPool({
+export const pool = createPool({
     host: process.env.MYSQLDB_HOST,
     user: 'root',
     password: process.env.MYSQLDB_ROOT_PASSWORD,
-    port: process.env.MYSQLDB_DOCKER_PORT
-})
+    port: process.env.MYSQLDB_DOCKER_PORT,
+    database: process.env.MYSQLDB_DATABASE 
+});
+
+app.use(express.json());
+
+const initializeDatabase = async () => {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL
+            )
+        `);
+        console.log("Tabla 'users' creada o ya existe.");
+    } catch (error) {
+        console.error('Error al inicializar la base de datos:', error);
+    }
+};
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo')
-})
-app.get('/ping', async(req, res) => {
-    const result = await pool.query('SELECT NOW()')
-    res.json(result[0])
-})
+    res.send('Hola');
+});
 
-app.listen(3000)
-console.log('Server on port', 3000)
+app.get('/ping', async (req, res) => {
+    const resultado = await pool.query('SELECT NOW()');
+    res.json(resultado[0]);
+});
+
+app.use('/auth', authRutas);
+
+app.listen(3000, async () => {
+    await initializeDatabase();
+    console.log('Servidor corriendo en el puerto', 3000);
+});
