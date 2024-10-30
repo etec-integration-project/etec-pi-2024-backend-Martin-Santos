7a88623a4813ddfb365eb395df0e66b31785ed60
Martin Santos <martin.santos.curadelli@gmail.com>
2024-10-30 20:39:33 -0300

    route corrected createProducts

--- src/rutas/rutasAutenticacion.js
+++ src/rutas/rutasAutenticacion.js
@@ -7,6 +7,6 @@ router.post('/registrar', registrar);
 router.post('/iniciar-sesion', iniciarSesion);
 router.get('/usuarios', listarUsuarios);  
 router.get('/productos', productos); 
-router.get('/createProducts', createProducts); // Crear y programar la funcion que se Encarge de guardar en la lista de productos, el producto nuevo ingresado por el usuario
+router.post('/createProducts', createProducts); // Crear y programar la funcion que se Encarge de guardar en la lista de productos, el producto nuevo ingresado por el usuario
 
 export default router;
