22ed1bb139ae20288dbd89e61416fb6eefbf150a
Martin-Santos <90261721+Martin-Santos@users.noreply.github.com>
2024-11-11 19:14:39 -0300

    changed editProduct url in rutasAutenticacion

--- src/rutas/rutasAutenticacion.js
+++ src/rutas/rutasAutenticacion.js
@@ -8,6 +8,6 @@ router.post('/iniciar-sesion', iniciarSesion);
 router.get('/usuarios', listarUsuarios);  
 router.get('/productos', productos); 
 router.post('/createProducts', createProducts); // Crear y programar la funcion que se Encarge de guardar en la lista de productos, el producto nuevo ingresado por el usuario
-router.patch('/editProduct'+id, updateProduct)
+router.patch('/editProduct/:id', updateProduct)
 
 export default router;
